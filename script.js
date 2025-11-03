d12ef4f63def4c908281b5dac0c636b346838b46
Vamshi Thallapalli <127075912+vamshithallapalli@users.noreply.github.com>
2025-11-03 13:04:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function validEmail(str) {
   //your JS code here.
+   if (!str) return false; // handle empty input
+
+  // Regular expression to validate email format
+  const emailRegex = /^[a-zA-Z0-9]+([._-]?[a-zA-Z0-9]+)*@[a-zA-Z0-9]+([.-]?[a-zA-Z0-9]+)*\.[a-zA-Z]{2,}$/;
+
+  return emailRegex.test(str);	
 }
 
 // Do not change the code below.
-const str = prompt("Enter an email address.");
-alert(validEmail(str));
+// const str = prompt("Enter an email address.");
+// alert(validEmail(str));
